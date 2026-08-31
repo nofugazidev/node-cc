d1da2893eedca3a5973776e4a3b32b1fa6a42dc1
nofugazidev <oyebisimubarak4@gmail.com>
2026-08-31 20:02:35 +0100

    added a logger middleware function

--- express/server.js
+++ express/server.js
@@ -1,36 +1,36 @@
 const express = require("express");
 const path = require("path");
-
+const { logger } = require("./middleware/logEvent");
 const app = express();
 const PORT = process.env.PORT || 3500;
-
-app.use(express.urlencoded({extended: false}));
-
+app.use(logger)
+//for urlencoded data
+app.use(express.urlencoded({ extended: false }));
+//for json files
 app.use(express.json())
-
-
-//for serving static files - css, images and all or anything in the public folder
+//built-in middleware for static files
 app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, '/js')))
 
-app.get(['/', '/index{.html}'], (req, res)=>{
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+app.get(["/", "/index{.html}"], (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
-app.get(['/about', '/about{.html}'], (req, res)=> {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'))
-})
+app.get(["/about", "/about{.html}"], (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "about.html"));
+});
 
-app.get(['/contact', '/contact{.html}'], (req, res)=> {
-    res.sendFile(path.join(__dirname, 'views', 'contact.html'))
-})
+app.get(["/contact", "/contact{.html}"], (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "contact.html"));
+});
 
-app.get(['/old-page', '/old-page{.html}'], (req, res) => {
-    res.redirect(301, '/index.html')
-})
+app.get(["/old-page", "/old-page{.html}"], (req, res) => {
+  res.redirect(301, "/index.html");
+});
 
-app.get('/*catchall', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-})
+app.get("/*catchall", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
