0fc237568b5d2afba0de644713edc0fbe4d1994c
nofugazidev <oyebisimubarak4@gmail.com>
2026-09-08 19:49:26 +0100

    cors foundation

--- express/server.js
+++ express/server.js
@@ -1,16 +1,38 @@
 const express = require("express");
 const path = require("path");
 const { logger } = require("./middleware/logEvent");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3500;
-app.use(logger)
+
+app.use(logger);
+
+const whitelist = [
+  "https://www.google.com",
+  "http://127.0.0.1:5500/",
+  "http://localhost:3500/",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Request Blocked by CORS!!!"))
+    }
+  },
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
 //for urlencoded data
 app.use(express.urlencoded({ extended: false }));
 //for json files
-app.use(express.json())
+app.use(express.json());
 //built-in middleware for static files
-app.use(express.static(path.join(__dirname, '/public')))
-app.use(express.static(path.join(__dirname, '/js')))
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/js")));
 
 app.get(["/", "/index{.html}"], (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
