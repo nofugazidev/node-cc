11aefab6fafe1b04603fa608989d08926abe7696
nofugazidev <oyebisimubarak4@gmail.com>
2026-09-12 10:27:44 +0100

    feat: added other API methods to my api

--- express/routes/apis/employee.js
+++ express/routes/apis/employee.js
@@ -1,12 +1,30 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const data = {}
+const data = {};
 
-data.employee = require('../../data/employee.json')
+data.employee = require("../../data/employee.json");
 
-router.route('/')
-    .get((req, res) => {
-        res.json(data.employee)
-    })
+router
+  .route("/")
+  .get((req, res) => {
+    res.json(data.employee);
+  })
+  .post((req, res) => {
+    res.json({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+    });
+  })
+  .put((req, res) => {
+    res.json({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+    });
+  })
+  .delete((req, res) => {
+    res.json({
+      id: req.body.id,
+    });
+  });
 
-    module.exports = router
+module.exports = router;
